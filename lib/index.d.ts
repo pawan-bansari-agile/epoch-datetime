6877c6cc80bb3673e4f641bb006d972497d7b4ef
pawan <pawan.bansari@agileinfoways.com>
2025-09-10 22:52:40 +0530

    resolving bugs

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,33 +1,42 @@
 import dayjs from 'dayjs';
 
 interface EpochConfig {
-    defaultFormat: string;
-    defaultTimezone: string;
-    use12Hour: boolean;
+    defaultTimezone?: string;
+    defaultFormat?: string;
+    use12Hour?: boolean;
 }
-declare function setConfig(newConfig: Partial<EpochConfig>): void;
-declare function getConfig(): EpochConfig;
-
-/** Convert input date+time+tz into epoch seconds */
-declare function toEpoch(date: string, time: string, tz?: string): number;
-/** Convert epoch back to date+time in given tz */
-declare function fromEpoch(epoch: number, tz?: string): {
+declare class EpochDateTime {
+    private config;
+    constructor(config?: EpochConfig);
+    toEpoch(date: string, time: string, tz?: string): number;
+    fromEpoch(epoch: number, tz?: string): {
+        date: string;
+        time: string;
+    };
+    formatEpoch(epoch: number, format?: string, tz?: string): string;
+    difference(epoch1: number, epoch2: number, unit?: dayjs.OpUnitType): number;
+    add(epoch: number, amount: number, unit: dayjs.ManipulateType): number;
+    subtract(epoch: number, amount: number, unit: dayjs.ManipulateType): number;
+    clamp(epoch: number, min: number, max: number): number;
+    round(epoch: number, unit: dayjs.OpUnitType): number;
+    startOfDay(epoch: number, tz?: string): number;
+    endOfDay(epoch: number, tz?: string): number;
+    isSameDay(epoch1: number, epoch2: number, tz?: string): boolean;
+}
+declare const setConfig: (cfg: EpochConfig) => void;
+declare const toEpoch: (date: string, time: string, tz?: string) => number;
+declare const fromEpoch: (epoch: number, tz?: string) => {
     date: string;
     time: string;
 };
+declare const formatEpoch: (epoch: number, format?: string, tz?: string) => string;
+declare const difference: (epoch1: number, epoch2: number, unit?: dayjs.OpUnitType) => number;
+declare const add: (epoch: number, amount: number, unit: dayjs.ManipulateType) => number;
+declare const subtract: (epoch: number, amount: number, unit: dayjs.ManipulateType) => number;
+declare const clamp: (epoch: number, min: number, max: number) => number;
+declare const round: (epoch: number, unit: dayjs.OpUnitType) => number;
+declare const startOfDay: (epoch: number, tz?: string) => number;
+declare const endOfDay: (epoch: number, tz?: string) => number;
+declare const isSameDay: (epoch1: number, epoch2: number, tz?: string) => boolean;
 
-declare function formatEpoch(epoch: number, format?: string, tz?: string): string;
-
-declare function convertTimezone(epoch: number, toTz: string): number;
-declare function startOfDay(epoch: number, tz?: string): number;
-declare function endOfDay(epoch: number, tz?: string): number;
-
-declare function isSameDay(epoch1: number, epoch2: number, tz?: string): boolean;
-declare function difference(epoch1: number, epoch2: number, unit?: dayjs.OpUnitType): number;
-
-declare function add(epoch: number, amount: number, unit: dayjs.ManipulateType): number;
-declare function subtract(epoch: number, amount: number, unit: dayjs.ManipulateType): number;
-declare function clamp(epoch: number, min: number, max: number): number;
-declare function round(epoch: number, unit: dayjs.OpUnitType): number;
-
-export { type EpochConfig, add, clamp, convertTimezone, difference, endOfDay, formatEpoch, fromEpoch, getConfig, isSameDay, round, setConfig, startOfDay, subtract, toEpoch };
+export { type EpochConfig, EpochDateTime, add, clamp, difference, endOfDay, formatEpoch, fromEpoch, isSameDay, round, setConfig, startOfDay, subtract, toEpoch };
